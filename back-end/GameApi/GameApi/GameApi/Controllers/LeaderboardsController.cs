ba6216d76fd36d62bf61a702166ea22d90cb09dd
Max <xgu830@aucklanduni.ac.nz>
2022-09-02 12:30:17 +1200

    leaderboard api

---
+++ back-end/GameApi/GameApi/GameApi/Controllers/LeaderboardsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GameApi.Data;
+using GameApi.Model;
+
+namespace GameApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LeaderboardsController : ControllerBase
+    {
+        private readonly GameDbContext _context;
+
+        public LeaderboardsController(GameDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetLeaderboards")]
+        public async Task<ActionResult<IEnumerable<Leaderboard>>> GetLeaderboards()
+        {
+            return await _context.Leaderboard.ToListAsync();
+        }
+    }
+}
