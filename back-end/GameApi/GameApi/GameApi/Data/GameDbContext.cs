73a13e70a4d6993cc5b46ea0bce9a682a9993f75
Max <xgu830@aucklanduni.ac.nz>
2022-09-02 20:01:34 +1200

    update backend

--- back-end/GameApi/GameApi/GameApi/Data/GameDbContext.cs
+++ back-end/GameApi/GameApi/GameApi/Data/GameDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using GameApi.Data.ContextConfigurations;
 using GameApi.Model;
 
 namespace GameApi.Data
@@ -8,16 +7,45 @@ namespace GameApi.Data
     {
         public GameDbContext (DbContextOptions options) : base(options)
         {
-
+            
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            var ids = new Guid[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
-
-            builder.ApplyConfiguration(new LeaderboardContextConfiguration(ids));
+            builder.Entity<Leaderboard>()
+                .HasData(
+                    new Leaderboard
+                    {
+                        Id = 1,
+                        Name = "Ooowl",
+                        Score = 2048
+                    },
+                    new Leaderboard
+                    {
+                        Id = 2,
+                        Name = "CoolGuy",
+                        Score = 1024
+                    },
+                    new Leaderboard
+                    {
+                        Id = 3,
+                        Name = "Toad",
+                        Score = 4096
+                    },
+                    new Leaderboard
+                    {
+                        Id = 4,
+                        Name = "PrincessPeach",
+                        Score = 3016
+                    },
+                    new Leaderboard
+                    {
+                        Id = 5,
+                        Name = "Mario",
+                        Score = 1048
+                    });
         }
 
-        public DbSet<Leaderboard> Leaderboard { get; set; }
+        public DbSet<Leaderboard> Leaderboards { get; set; }
     }
 }
